01b15941f4e3d2694f21e60ccba6a921119bd902
Carlos Pérez <carlos.perez@a2system.net>
2015-11-11 13:05:25 +0100

    -mod: delete contact detail

--- app/pods/components/button-delete-contact/component.js
+++ app/pods/components/button-delete-contact/component.js
@@ -3,9 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	actions:{
 		deleteContact:function(model){
-			model.deleteRecord();
-			model.save();
+			// model.deleteRecord();
+			// model.save();
+			this.sendAction('actionDel', model);
 		}
 	}
-	
+
 });

1667af77d7d81b2525349c6e5c39d5a9c1058461
Carlos Pérez <carlos.perez@a2system.net>
2015-11-13 10:28:08 +0100

    -mod: añadidos comentarios

--- app/helpers/input-helper.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function inputHelper(model, attr) {
-    return Ember.get(model, attr);
-}
-
-export default Ember.Helper.helper(inputHelper);
--- app/pods/components/input-binder/component.js
+++ app/pods/components/input-binder/component.js
@@ -6,13 +6,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName:'spam',
 	prop: null, // passed in
-	obj: null,   // passed in
+	model: null,   // passed in
 	value: null,    // local
 
-	onValue: function() {
-		var obj = this.get('obj');
+	// onValue: function() {
+	// 	var model = this.get('model');
+	// 	var prop = this.get('prop');
+	// 	var value = this.get('value');
+	// 	Ember.set(model, prop, value);
+	// }.observes('value'),
+	onValue: Ember.observer('value', function() {
+		var model = this.get('model');
 		var prop = this.get('prop');
 		var value = this.get('value');
-		Ember.set(obj, prop, value);
-	}.observes('value'),
+		Ember.set(model, prop, value);
+	})
 });

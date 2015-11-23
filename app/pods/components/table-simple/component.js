1ac65fbcc970a1f37c0c0439f274352402c0ee7d
Carlos Pérez <carlos.perez@a2system.net>
2015-11-23 17:07:25 +0100

    -mod: modificado algoritmo de paginación para mostrar ... a partir de 8 páginas

--- app/pods/components/table-simple/component.js
+++ app/pods/components/table-simple/component.js
@@ -336,34 +336,42 @@ export default Ember.Component.extend({
 			pagesCount,
 			currentPageNumber
 		} = getProperties(this, 'pagesCount', 'currentPageNumber');
-		const notLinkLabel = '...';
-		var groups = []; // array of 8 numbers
-		var labels = A([]);
-		groups[0] = 1;
-		groups[1] = Math.min(1, pagesCount);
-		groups[6] = Math.max(1, pagesCount);
-		groups[7] = pagesCount;
-		groups[3] = Math.max(groups[1] + 1, currentPageNumber - 1);
-		groups[4] = Math.min(groups[6] - 1, currentPageNumber + 1);
-		groups[2] = Math.floor((groups[1] + groups[3]) / 2);
-		groups[5] = Math.floor((groups[4] + groups[6]) / 2);
 
-		for (let n = groups[0]; n <= groups[1]; n++) {
-			labels[n] = n;
-		}
-		const userGroup2 = groups[4] >= groups[3] && ((groups[3] - groups[1]) > 1);
-		if (userGroup2) {
-			labels[groups[2]] = notLinkLabel;
-		}
-		for (let i = groups[3]; i <= groups[4]; i++) {
-			labels[i] = i;
-		}
-		const userGroup5 = groups[4] >= groups[3] && ((groups[6] - groups[4]) > 1);
-		if (userGroup5) {
-			labels[groups[5]] = notLinkLabel;
-		}
-		for (let i = groups[6]; i <= groups[7]; i++) {
-			labels[i] = i;
+		var labels = A([]);
+		const notLinkLabel = '...';
+
+		if ( pagesCount <= 7 ) {
+			for (let n = 0; n<pagesCount; n++ ) {
+				labels[n] = n+1;
+			}
+		} else {
+			var groups = []; // array of 8 numbers
+			groups[0] = 1;
+			groups[1] = Math.min(1, pagesCount);
+			groups[6] = Math.max(1, pagesCount);
+			groups[7] = pagesCount;
+			groups[3] = Math.max(groups[1] + 1, currentPageNumber - 1);
+			groups[4] = Math.min(groups[6] - 1, currentPageNumber + 1);
+			groups[2] = Math.floor((groups[1] + groups[3]) / 2);
+			groups[5] = Math.floor((groups[4] + groups[6]) / 2);
+
+			for (let n = groups[0]; n <= groups[1]; n++) {
+				labels[n] = n;
+			}
+			const userGroup2 = groups[4] >= groups[3] && ((groups[3] - groups[1]) > 1);
+			if (userGroup2) {
+				labels[groups[2]] = notLinkLabel;
+			}
+			for (let i = groups[3]; i <= groups[4]; i++) {
+				labels[i] = i;
+			}
+			const userGroup5 = groups[4] >= groups[3] && ((groups[6] - groups[4]) > 1);
+			if (userGroup5) {
+				labels[groups[5]] = notLinkLabel;
+			}
+			for (let i = groups[6]; i <= groups[7]; i++) {
+				labels[i] = i;
+			}
 		}
 		return A(labels.compact().map(label => { return {
 			label: label,
